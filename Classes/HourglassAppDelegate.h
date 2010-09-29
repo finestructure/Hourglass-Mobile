c32674c011eea89f8d03b8ee8cb36ec5d0150f38
Sven A. Schmidt <sas@abstracture.de>
2010-09-29 16:03:51 +0200

    Initial template.

---
+++ Classes/HourglassAppDelegate.h
@@ -0,0 +1,26 @@
+//
+//  HourglassAppDelegate.h
+//  Hourglass
+//
+//  Created by Sven A. Schmidt on 29.09.10.
+//  Copyright (c) 2010 abstracture GmbH & Co. KG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HourglassAppDelegate : NSObject <UIApplicationDelegate> {
+  UIWindow *window;
+  NSManagedObjectContext *managedObjectContext;
+  NSManagedObjectModel *managedObjectModel;
+  NSPersistentStoreCoordinator *persistentStoreCoordinator;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (NSString *)applicationDocumentsDirectory;
+
+@end
---
+++ main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Hourglass
+//
+//  Created by Sven A. Schmidt on 29.09.10.
+//  Copyright (c) 2010 abstracture GmbH & Co. KG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+  NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+  int retVal = UIApplicationMain(argc, argv, nil, nil);
+  [pool release];
+  return retVal;
+}
