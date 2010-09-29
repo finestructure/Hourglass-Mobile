fe4a297b7aaff364f6cd0a1e4f9f83a5c67bf721
Sven A. Schmidt <sas@abstracture.de>
2010-09-29 16:36:58 +0200

    Added TaskViewController

---
+++ Classes/TaskViewController.h
@@ -0,0 +1,16 @@
+//
+//  TaskViewController.h
+//  Hourglass
+//
+//  Created by Sven A. Schmidt on 29.09.10.
+//  Copyright (c) 2010 abstracture GmbH & Co. KG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface TaskViewController : UITableViewController {
+    
+}
+
+@end
